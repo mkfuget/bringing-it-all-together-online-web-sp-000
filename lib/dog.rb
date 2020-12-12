292b737ae141c9552cddc806b25bd635ac886773
Michael Fuget <michaelfuget@gmail.com>
2020-12-12 21:26:11 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,21 +7,5 @@ class Dog
     @id = data_hash[:id] 
   end 
   
-  def self.create_table
-    drop_table    
-    sql_create = <<-SQL
-      CREATE TABLE dogs
-    SQL
-    
-    DB[:conn].execute(sql_create)
-  end 
-  
-  def self.drop_table
-    sql_drop = <<-SQL
-      DROP TABLE IF EXISTS dogs
-    SQL
-    
-    DB[:conn].execute(sql_drop)
-  end
     
 end
