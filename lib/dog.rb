8324191115c3808597c664017b2648ef60db6873
Michael Fuget <michaelfuget@gmail.com>
2020-12-12 21:04:11 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,27 +1,5 @@
 class Dog 
   attr_accessor :name, :breed, :id 
   
-  def initialize(name, breed, id=nil)
-    @name = name 
-    @breed = breed 
-    @id = id 
-  end 
-  
-  def self.create_table
-    drop_table    
-    sql_create = <<-SQL
-      CREATE TABLE dogs
-    SQL
-    
-    DB[:conn].execute(sql_create)
-  end 
-  
-  def self.drop_table
-    sql_drop = <<-SQL
-      DROP TABLE IF EXISTS dogs
-    SQL
-    
-    DB[:conn].execute(sql_drop)
-  end
     
 end
